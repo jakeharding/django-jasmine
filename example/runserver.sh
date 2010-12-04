9e433ee9fa9b1677f827efb5ef07dc33f6602fb0
Jonathan McCoy <jonathan@perceptionlabs.com>
2010-12-03 17:26:15 -0800

    removing extra files

--- example/runserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-PYTHONPATH=`pwd`:`pwd`/..:$PYTHONPATH
-django-admin.py runserver --settings=settings 8080
--- example/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url('^qunit/', include('django_qunit.urls'))
-)
