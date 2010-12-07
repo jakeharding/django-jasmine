1e973cdcdf5b5bf0e688e8bc019c0f0c14b4c4c1
Jonathan McCoy <jonathan@perceptionlabs.com>
2010-12-06 16:07:59 -0800

    small edit of url pattern

--- django_jasmine/urls.py
+++ django_jasmine/urls.py
@@ -7,13 +7,13 @@ urlpatterns = patterns('',
     url(r'^tests/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.JASMINE_TEST_DIRECTORY+'/spec/',
     }, name='jasmine_test'),
-    url(r'^jasmine/jasmine.js', 'django.views.static.serve', {
+    url(r'^jasmine.js', 'django.views.static.serve', {
         'document_root': media_root, 'path': 'jasmine-1.0.1/jasmine.js',
     }, name='jasmine_js'),
-    url(r'^jasmine/jasmine-html.js', 'django.views.static.serve', {
+    url(r'^jasmine-html.js', 'django.views.static.serve', {
         'document_root': media_root, 'path': 'jasmine-1.0.1/jasmine-html.js',
     }, name='jasmine_html_js'),
-    url(r'^jasmine/jasmine.css', 'django.views.static.serve', {
+    url(r'^jasmine.css', 'django.views.static.serve', {
         'document_root': media_root, 'path': 'jasmine-1.0.1/jasmine.css',
     }, name='jasmine_css'),
     url('^(?P<path>.*)$', 'django_jasmine.views.run_tests',
